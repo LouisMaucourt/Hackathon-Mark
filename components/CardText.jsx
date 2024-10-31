8b09079fd4776bc522b18c38e88782db9523b3fc
Louis-maucourt <louis.maucourt7@gmail.com>
2024-10-31 02:52:54 +0100

    wip

--- components/CardText.jsx
+++ components/CardText.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Cardtext = () => {
     return (
@@ -7,7 +7,7 @@ const Cardtext = () => {
             <div className='flex gap-8'>
                 <div className='border bg-[#252723] border-[var(--f-color)] p-4 flex flex-col gap-4 rounded-2xl w-1/3' >
                     <h4>Technologie de pointe anti-système</h4>
-                    <p>Pas de Wi-Fi, pas de surveillance ! Avec le Markbook, vous naviguez dans l'ombre.</p>
+                    <p>Pas de Wi-Fi, pas de surveillance ! Avec le Markbook, vous naviguez dans l&apos;ombre.</p>
                 </div>
                 <div className='border bg-[#252723] border-[var(--f-color)] p-4 flex flex-col gap-4 rounded-2xl w-1/3'>
                     <h4>Navigateur TOR intégré</h4>
@@ -18,9 +18,8 @@ const Cardtext = () => {
                     <p>Écrivez librement avec un clavier traditionnel. Ici, chaque touche compte, sans influence extérieure. Vous êtes maître de vos mots. </p>
                 </div>
             </div>
-
         </section >
     )
 }
 
-export default Cardtext
+export default Cardtext;
